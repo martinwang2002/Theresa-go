daea8109dc30e40627367ef7fcafa8fef6da1fd5
Martin <martinwang2002@outlook.com>
2022-08-22 01:08:32 +0000

    fix(akabfs): error handling for cacheManager.Set

--- internal/akAbFs/akAbFs.go
+++ internal/akAbFs/akAbFs.go
@@ -11,6 +11,7 @@ import (
 	pathLib "path"
 	"sort"
 	"strings"
+	"time"
 
 	"github.com/allegro/bigcache/v3"
 	"github.com/eko/gocache/v3/cache"
@@ -40,9 +41,46 @@ func NewAkAbFs() *AkAbFs {
 	if err != nil {
 		panic(err)
 	}
-	bigcacheClient, _ := bigcache.NewBigCache(bigcache.Config{
+	bigcacheClient, err := bigcache.NewBigCache(bigcache.Config{
+		// number of shards (must be a power of 2)
+		Shards: 1024,
+
+		// time after which entry can be evicted
+		LifeWindow: 2 * time.Minute,
+
+		// Interval between removing expired entries (clean up).
+		// If set to <= 0 then no action is performed.
+		// Setting to < 1 second is counterproductive — bigcache has a one second resolution.
+		CleanWindow: 1 * time.Second,
+
+		// rps * lifeWindow, used only in initial memory allocation
+		MaxEntriesInWindow: 1000 * 10 * 60,
+
+		// max entry size in bytes, used only in initial memory allocation
+		MaxEntrySize: 500,
+
+		// prints information about additional memory allocation
 		Verbose: false,
+
+		// cache will not allocate more memory than this limit, value in MB
+		// if value is reached then the oldest entries can be overridden for the new ones
+		// 0 value means no size limit
+		HardMaxCacheSize: 128,
+
+		// callback fired when the oldest entry is removed because of its expiration time or no space left
+		// for the new entry, or because delete was called. A bitmask representing the reason will be returned.
+		// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+		OnRemove: nil,
+
+		// OnRemoveWithReason is a callback fired when the oldest entry is removed because of its expiration time or no space left
+		// for the new entry, or because delete was called. A constant representing the reason will be passed through.
+		// Default value is nil which means no callback and it prevents from unwrapping the oldest entry.
+		// Ignored if OnRemove is specified.
+		OnRemoveWithReason: nil,
 	})
+	if err != nil {
+		panic(err)
+	}
 	bigcacheStore := store.NewBigcache(bigcacheClient)
 
 	cacheManager := cache.New[[]byte](bigcacheStore)
@@ -188,7 +226,10 @@ func (akAbFs *AkAbFs) List(path string) (JsonDirEntries, error) {
 	var buffer bytes.Buffer
 	err = gob.NewEncoder(&buffer).Encode(jsonEntries)
 	if err == nil {
-		akAbFs.cacheManager.Set(akAbFs.akAbFsContext, "List"+path, buffer.Bytes())
+		err = akAbFs.cacheManager.Set(akAbFs.akAbFsContext, "List"+path, buffer.Bytes())
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 	return jsonEntries, nil
 }
@@ -254,8 +295,10 @@ func (akAbFs *AkAbFs) NewJsonObject(path string) (*gjson.Result, error) {
 	}
 	defer ObjectIoReader.Close()
 
-	akAbFs.cacheManager.Set(akAbFs.akAbFsContext, "NewJsonObject"+path, ObjectIoReaderBytes)
-	
+	err = akAbFs.cacheManager.Set(akAbFs.akAbFsContext, "NewJsonObject"+path, ObjectIoReaderBytes)
+	if err != nil {
+		fmt.Println(err)
+	}
 	gjsonResult := gjson.ParseBytes(ObjectIoReaderBytes)
 
 	return &gjsonResult, nil
